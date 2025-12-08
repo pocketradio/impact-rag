2c6ff5c14159947d1b4eac9e70182532448e517b
cloudwolf <206946888+xerxes324@users.noreply.github.com.>
2025-12-08 17:15:48 +0530

    chunking the nodes returned from parser with 30 line threshold

---
+++ rag-service/pipeline/chunking.py
@@ -0,0 +1,44 @@
+def make_chunks(nodes):
+    
+    all_chunks = []
+    file_cache = {}         # stores code lines from each file 
+        
+    for node in nodes:
+    
+        if node["file"] not in file_cache:
+            with open(node["file"], "r",encoding='utf-8') as f:
+                file_cache[node["file"]] =  f.read().splitlines()  #filecache key -> filename , value -> array of all code lines
+        
+        file_text = file_cache[node["file"]]
+
+        start = node["lineno"]-1
+        end = node["endlineno"]
+        size = node["endlineno"] - node["lineno"] + 1
+        
+        lines = file_text[start:end]
+        
+        if size > 30:
+                
+            for i in range(0, size, 30):
+                
+                sub_chunk = lines[i:i+30]
+                
+                all_chunks.append({
+                    "code" : "\n".join(sub_chunk), 
+                    "file" : node["file"],
+                    "name" : node["name"],
+                    "type" : node["type"]
+                })
+            continue
+        
+        
+        code = "\n".join(lines)
+        
+        all_chunks.append({
+            "code" : code, 
+            "file" : node["file"],
+            "name" : node["name"],
+            "type" : node["type"]
+        })
+
+    return all_chunks
