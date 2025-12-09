ca61d8ee62b9ee9d475900deeb874474274f08b6
cloudwolf <206946888+xerxes324@users.noreply.github.com.>
2025-12-09 10:30:06 +0530

    added function for query collection for the retriever pipeline

--- rag-service/pipeline/vectorstore.py
+++ rag-service/pipeline/vectorstore.py
@@ -1,20 +1,11 @@
 import chromadb
 import os
 
+client = chromadb.PersistentClient(path="chroma_db")
+collection = client.get_or_create_collection("code_chunks")
+
 def chromaDBstorage(chunks):
     os.makedirs("chroma_db", exist_ok = True)
-    
-    client = chromadb.PersistentClient(path="chroma_db")
-    
-    
-    # try:
-    #     client.delete_collection("code_chunks")
-    # except:
-    #     print('no collection to delete. Proceeding...')
-    
-    
-    collection = client.create_collection("code_chunks")
-    
     
     ids = [] 
     embeddings = []
@@ -41,5 +32,29 @@ def chromaDBstorage(chunks):
         embeddings=embeddings,
         documents=documents
     )
+    # print(type(collection[0], 'is the collection 0 type'))
     
+
+def query_collection(embedded_query, top_k, repo_id):
+    results = collection.query(
+        query_embeddings = [embedded_query],
+        where = {
+            "repo_id" : repo_id
+        },
+        n_results = top_k
+    )
     
+    # print("\n\n\n\n ", type(results[0]), 'is the type of results ')
+    
+    return [
+		{
+			"id": id,
+			"document": doc,
+			"metadata": meta,
+		}
+		for id, doc, meta in zip(
+			results["ids"][0],
+			results["documents"][0],
+			results["metadatas"][0],
+		)
+	]
