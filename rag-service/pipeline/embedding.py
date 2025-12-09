5f81a228b121573e1cdcd2f42cc24607ddefea49
cloudwolf <206946888+xerxes324@users.noreply.github.com.>
2025-12-09 10:24:44 +0530

    changes made for debugging and testing

--- rag-service/pipeline/embedding.py
+++ rag-service/pipeline/embedding.py
@@ -6,5 +6,13 @@ def embedding_pipeline(chunks : List):
 
     for chunk in chunks:
         chunk["embedding"] = model.encode(chunk["code"]).tolist() # since encode returns numpY array ( not json serializable)
-    
+
+    print(type(chunks))
+    # print(type(chunks[0]),"\n\n\n" ,chunks[0]) 
     return chunks
+    # each CHUNK is a dictionary with code, type, name, embeddings, etc. 
+    # chunks is a list. 
+
+def embed_query(query : str):
+    embedded_query = model.encode(query).tolist()
+    return embedded_query
