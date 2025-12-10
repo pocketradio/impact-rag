2c036ab2cebe47b565f0d9679553419c28185628
cloudwolf <206946888+xerxes324@users.noreply.github.com.>
2025-12-10 16:47:02 +0530

    refactor : chunking based on code modules instead of lines of code to retain context

--- rag-service/pipeline/chunking.py
+++ rag-service/pipeline/chunking.py
@@ -1,49 +1,119 @@
 from uuid import uuid4
+import ast
 def make_chunks(nodes, repo_id):
     
-    all_chunks = []
-    file_cache = {}         # stores code lines from each file 
-        
-    for node in nodes:
+
+    chunks = []
+    file_cache = {}
     
+    
+    for node in nodes:
+        
+        start = node["line_no"] - 1
+        end = node["end_line_no"]
+        size = end - start
+        
         if node["file"] not in file_cache:
             with open(node["file"], "r",encoding='utf-8') as f:
-                file_cache[node["file"]] =  f.read().splitlines()  #filecache key -> filename , value -> array of all code lines
-        
-        file_text = file_cache[node["file"]]
-
-        start = node["lineno"]-1
-        end = node["endlineno"]
-        size = node["endlineno"] - node["lineno"] + 1
-        
-        lines = file_text[start:end]
-        
-        if size > 30:
+                file_cache[node["file"]] =  f.read().splitlines()
                 
-            for i in range(0, size, 30):
-                
-                sub_chunk = lines[i:i+30]
-                
-                all_chunks.append({
-                    "code" : "\n".join(sub_chunk), 
-                    "file" : node["file"],
+        code = "\n".join(file_cache[node["file"]][start : end])
+        
+        if node["type"] == 'function':
+            # code = "\n".join(file_cache[node["file"]][start : end])
+            chunks.append({
+                "metadata" : {
+                    "type" : "function",
+                    "repo_id" : repo_id,      
                     "name" : node["name"],
-                    "type" : node["type"],
                     "id" : str(uuid4()),
-                    "repo_id" : repo_id
-                })
-            continue
-        
-        
-        code = "\n".join(lines)
-        
-        all_chunks.append({
-            "code" : code, 
-            "file" : node["file"],
-            "name" : node["name"],
-            "type" : node["type"],
-            "id" : str(uuid4()),
-            "repo_id" : repo_id
-        })
+                    "file" : node["file"],
+                },
 
-    return all_chunks
+                "code" : code,
+            })
+            
+        elif node["type"] == 'method':
+            # code = "\n".join(file_cache[node["file"]][start : end])
+            
+            chunks.append({
+                "metadata" : {
+                    "type" : "method",
+                    "repo_id" : repo_id,
+                    "name" : node["name"],
+                    "id" : str(uuid4()),       
+                    "class": node["class"],
+                    "file" : node["file"],                
+                },
+                
+                
+                "code" : f"\nclass {node['class']}:\n{code}",
+            })
+
+        elif node["type"] == "class":
+
+            chunks.append({
+                "metadata" :{
+                    "type" : "class_summary",
+                    "repo_id" : repo_id,
+                    "name" : node["name"],
+                    "id" : str(uuid4()),  
+                    "file" : node["file"],
+                },
+                
+                "code": "",
+                "methods" : node["methods"]
+            })
+            
+        
+            
+            
+            
+    # all_chunks = []
+    # file_cache = {}         # stores code lines from each file 
+        
+    # for node in nodes:
+    
+    #     if node["file"] not in file_cache:
+    #         with open(node["file"], "r",encoding='utf-8') as f:
+    #             file_cache[node["file"]] =  f.read().splitlines()  #filecache key -> filename , value -> array of all code lines
+        
+    #     file_text = file_cache[node["file"]]
+
+    #     start = node["lineno"]-1
+    #     end = node["endlineno"]
+    #     size = node["endlineno"] - node["lineno"] + 1
+        
+    #     lines = file_text[start:end]
+        
+    #     MAX_LINES = 8
+        
+    #     if size > MAX_LINES:
+                
+    #         for i in range(0, size, MAX_LINES):
+                
+    #             sub_chunk = lines[i:i+MAX_LINES]
+                
+    #             all_chunks.append({
+    #                 "code" : "\n".join(sub_chunk), 
+    #                 "file" : node["file"],
+    #                 "name" : node["name"],
+    #                 "type" : node["type"],
+    #                 "id" : str(uuid4()),
+    #                 "repo_id" : repo_id
+    #             })
+    #         continue
+        
+        
+    #     code = "\n".join(lines)
+        
+    #     all_chunks.append({
+    #         "code" : code, 
+    #         "file" : node["file"],
+    #         "name" : node["name"],
+    #         "type" : node["type"],
+    #         "id" : str(uuid4()),
+    #         "repo_id" : repo_id
+    #     })
+
+    # return all_chunks
